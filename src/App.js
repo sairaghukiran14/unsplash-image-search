b13cdbd6f703a3bce1d0599ef3da28242888f638
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2022-12-27 17:53:13 +0530

    realtime

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
-import React,{useState} from 'react';
+import React,{ useEffect,useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
   const [image,setimage] = useState([]);
-  let api01="https://api.unsplash.com/search/collections?page=1&per_page=25&orientation=squarish&query=";
+  const [page,setpage] = useState(1);
+
+  const hello=(page,keyword)=>{
+    return `https://api.unsplash.com/search/collections?page=${page}1&per_page=15&orientation=squarish&query=${keyword}&client_id=dVso0OdCflrQXO9sq9wp5FtmYBltKMqpm6CZziE3sPM`
+  }
+  // const api01="https://api.unsplash.com/search/collections?page="+{page}+"1&per_page=15&orientation=squarish&query=";
   const [keyword,setkeyword]=useState('');
-  let api02="&client_id=dVso0OdCflrQXO9sq9wp5FtmYBltKMqpm6CZziE3sPM";
+  // const api02="&client_id=dVso0OdCflrQXO9sq9wp5FtmYBltKMqpm6CZziE3sPM";
   
   const getImage=()=>{
-    axios.get(api01+keyword+api02)
+    axios.get(hello(page, keyword))
     .then((response)=>{
       setimage(response.data.results);
     })
   }
 
+  useEffect(()=>{
+    getImage();
+  })
+
   const changehandler=(e)=>{
     e.preventDefault();
-    
     setkeyword(e.target.value);
-    
+  }
+  const prevpage=()=>{
+    if(page>1)
+    {
+      setpage(page-1);
+      getImage();
+    }
+  }
+  const nextpage=()=>{
+    setpage(page+1);
+    getImage();
   }
   return (
     <div className="App">
       <div className='searchfield'>
       <h2>Unsplash Image Search </h2>
-      <form onSubmit={changehandler}>
+      <form onSubmit="">
 
       <input type="text" placeholder="Enter the keyword" value={keyword} onChange={changehandler}/>
-      <button onClick={getImage}>Search</button>
+      <button onClick={getImage} type="button">Search</button>
       </form>
       </div>
       
-
-
       <div className='imagedisplay'>
         {
           image.map((value,index)=>{
@@ -47,15 +63,19 @@ function App() {
                   <img src={value.user.profile_image.medium} className="userpro" alt='userlogo'/>
                   <div className='likes00'>{value.user.total_likes}<img src='like.png' alt='likes'/></div>
                   <div className='name'>{value.user.username}</div>
+                  <a href={value.cover_photo.urls.small} download><button>Download</button></a>
                 </div>
               </div>
             )
           })
         }
       </div>
+      <div className='pagination'>
+        <button onClick={prevpage}>Previous Page</button>
+        {page}
+        <button onClick={nextpage}>Next page</button>
+      </div>
     </div>
-    
   );
 }
-
 export default App;
